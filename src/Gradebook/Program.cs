9ef702e3790853331176342c06f65ccf22e259af
Jhamere Wilson <jhamerewilson@Jhameres-MacBook-Pro.local>
2020-05-19 12:51:28 -0400

    initial commit

---
+++ src/Gradebook/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Gradebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new[] { 12.7, 6.3, 6.11 };
+            var result = 0.0;
+            foreach (double number in numbers)
+            {
+                result += number; // number + number
+
+            }
+            Console.WriteLine("The result is " + result);
+        }
+    }
+}
